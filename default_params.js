79e0d8c9d0e1f3c3b479b114b4cfbd4d28203792
Mottaqui Karim <taqqui.karim@gmail.com>
2018-10-08 22:05:52 -0400

    more examples

--- default_params.js
+++ default_params.js
@@ -9,3 +9,20 @@ console.log(addThreeNums(2,1))  // 3
 console.log(addThreeNums(2))  // 2
 // a = undefined, b = undefined, c = undefined
 console.log(addThreeNums()) // 0 
+
+/*
+    PROBLEM 1:
+    Create a function called getFullName that takes three arguments
+    (one for firstName, one for middleName, one for lastName)
+    
+    use a default parameter for the middleName, set it to ""
+*/
+
+function getFullName(firstName, lastName, middleName = "") {
+    //return firstName + " " + middleName + " " + lastName;
+    return `${firstName} ${middleName} ${lastName}`;
+}
+
+console.log(getFullName("Mottaqui", "Karim", "Al"))
+
+
